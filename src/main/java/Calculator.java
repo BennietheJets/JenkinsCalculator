2f4394f3aa7afa5a7548879221e0aab33128f9c5
broad <mrbenjammin95@gmail.com>
2021-02-02 12:54:02 -0700

    fix Fibo

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -41,8 +41,10 @@ class Calculator {
     int fibonacciNumberFinder(int n){
         int t1 = 0, t2 = 1;
 
+        int sum = 0;
+
         for (int i = 1; i <= n; ++i) {
-            int sum = t1 + t2;
+            sum = t1 + t2;
             t1 = t2;
             t2 = sum;
         }
